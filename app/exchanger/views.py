1d06c26233da975992455bd4e3439f12f8d14967
GooseOfWar <marsealergei@gmail.com>
2022-07-20 17:04:31 +0300

    Project sketch was made

--- app/exchanger/views.py
+++ app/exchanger/views.py
@@ -7,7 +7,13 @@ from exchanger.models import ContactUs
 def contact_us(request):  # pylint: disable=unused-argument
     contact_us_list: list = []
     for contact_case in ContactUs.objects.all():
-        html_str: str = f'<p>ID: {contact_case.id} FROM: {contact_case.email_from}    TO: {contact_case.email_to}</p>'
+        html_str: str = f'\
+        <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">\
+        <p> MESSAGE ID: {contact_case.id}</p>\
+        <p style="margin-left: 30px;">FROM: {contact_case.email_from}</p>\
+        <p style="margin-left: 30px;">TO: {contact_case.email_to}</p>\
+        <p style="margin-left: 30px;">SUBJECT: {contact_case.subject}</p>\
+        <p style="margin-left: 50px;">{contact_case.message}</p>'
         contact_us_list.append(html_str)
         result: str = str(contact_us_list).replace("', '", '')
     return HttpResponse(result)  # noqa
